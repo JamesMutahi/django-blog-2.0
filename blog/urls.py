f1aa281480b828d7a6ce042f4c2ef6503b27d2ff
JamesMutahi <mutahijames0@gmail.com>
2019-11-14 11:32:19 +0300

    Add custom template tags using simple and inclusion tags to show total, latest and most commented posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name = 'blog'
@@ -6,9 +7,9 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     # url(r'^$', views.PostListView.as_view(), name='post_list'),
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$', views.post_detail,
-        name='post_detail'),
-    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$', views.post_detail,
+            name='post_detail'),
+    re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
 ]
