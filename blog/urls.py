00a8d064a15c761ef054c9824be07da6dfa1d68b
JamesMutahi <mutahijames0@gmail.com>
2019-11-14 12:13:54 +0300

    Convert re_path to use path

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,19 @@
-from django.conf.urls import url
-from django.urls import re_path
+from django.urls import re_path, path
 from . import views
 
 app_name = 'blog'
 
+# urlpatterns = [
+#     re_path(r'^$', views.post_list, name='post_list'),
+#     re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+#     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$', views.post_detail,
+#             name='post_detail'),
+#     re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+# ]
+
 urlpatterns = [
-    # post views
-    # url(r'^$', views.PostListView.as_view(), name='post_list'),
-    re_path(r'^$', views.post_list, name='post_list'),
-    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
-    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'r'(?P<post>[-\w]+)/$', views.post_detail,
-            name='post_detail'),
-    re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
